e2b1d87238e40ac2d2e67ce1a293ef778952b0f5
alex-heville <cinziaannaso8839@hotmail.com>
2025-12-28 04:22:01 +0800

    Add full_adder module

---
+++ src/adder/full_adder.v
@@ -0,0 +1,11 @@
+// Full Adder module
+module full_adder (
+    input a,
+    input b,
+    input cin,
+    output sum,
+    output cout
+);
+    assign sum = a ^ b ^ cin;
+    assign cout = (a & b) | (b & cin) | (cin & a);
+endmodule
